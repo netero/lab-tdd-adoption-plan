113242f27077fc7687cb7392b803ab1119b52904
Rafael Morales <rmorales@aligare.cl>
2020-01-15 12:08:17 -0300

    Resuelta toda la libreria

--- src/main/java/cl/aligare/tdd/adoption_plan/IVA.java
+++ src/main/java/cl/aligare/tdd/adoption_plan/IVA.java
@@ -4,4 +4,12 @@ public class IVA {
 	public int getTax(int neto) {
 		return (int)Math.round(neto*0.19);
 	}
+	
+	public int getAmountWithTax(int neto) {
+		return (int)Math.round(neto*1.19);
+	}
+	
+	public int getAmountWithoutTax(int total) {
+		return (int)Math.round(total/1.19);
+	}
 }
--- src/test/java/cl/aligare/tdd/adoption_plan/IVATest.java
+++ src/test/java/cl/aligare/tdd/adoption_plan/IVATest.java
@@ -22,4 +22,22 @@ public class IVATest {
 		assertEquals(2714, iva.getTax(14286));
 		assertEquals(2395, iva.getTax(12605));
 	}
+	
+	@Test
+	public void obtenerMontoConIVA() {
+		assertEquals(62000, iva.getAmountWithTax(52101));
+		assertEquals(30000, iva.getAmountWithTax(25210));
+		assertEquals(19000, iva.getAmountWithTax(15966));
+		assertEquals(17000, iva.getAmountWithTax(14286));
+		assertEquals(15000, iva.getAmountWithTax(12605));
+	}
+	
+	@Test
+	public void obtenerNeto() {
+		assertEquals(52101, iva.getAmountWithoutTax(62000));
+		assertEquals(25210, iva.getAmountWithoutTax(30000));
+		assertEquals(15966, iva.getAmountWithoutTax(19000));
+		assertEquals(14286, iva.getAmountWithoutTax(17000));
+		assertEquals(12605, iva.getAmountWithoutTax(15000));
+	}
 }
