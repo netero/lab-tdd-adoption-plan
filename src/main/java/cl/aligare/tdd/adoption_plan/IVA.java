3f2c95bb46810e7ae974b84d75e36344e09001ee
Rafael Morales <rmorales@aligare.cl>
2020-01-15 11:27:17 -0300

    Primera funcionalidad resuelta con su prueba

--- src/main/java/cl/aligare/tdd/adoption_plan/IVA.java
+++ src/main/java/cl/aligare/tdd/adoption_plan/IVA.java
@@ -1,5 +1,7 @@
 package cl.aligare.tdd.adoption_plan;
 
 public class IVA {
-
+	public int getTax(int neto) {
+		return (int)Math.round(neto*0.19);
+	}
 }
--- src/test/java/cl/aligare/tdd/adoption_plan/IVATest.java
+++ src/test/java/cl/aligare/tdd/adoption_plan/IVATest.java
@@ -1,11 +1,25 @@
 package cl.aligare.tdd.adoption_plan;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class IVATest {
 	
-	@Test
+	IVA iva;
+	
+	@Before
 	public void classGeneration() {
-		IVA iva= new IVA();
+		iva= new IVA();
+	}
+	
+	@Test
+	public void obtenerImpuestoDesdeMontoNeto() {
+		assertEquals(9899, iva.getTax(52101));
+		assertEquals(4790, iva.getTax(25210));
+		assertEquals(3034, iva.getTax(15966));
+		assertEquals(2714, iva.getTax(14286));
+		assertEquals(2395, iva.getTax(12605));
 	}
 }
