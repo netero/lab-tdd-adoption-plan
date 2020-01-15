9d7d4f46994d3b247475feea4f1ebbe6af4768aa
Rafael Morales <rmorales@aligare.cl>
2020-01-15 10:22:18 -0300

    Proyecto maven inicial para el laboratorio

---
+++ src/main/java/cl/aligare/tdd/adoption_plan/App.java
@@ -0,0 +1,13 @@
+package cl.aligare.tdd.adoption_plan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
