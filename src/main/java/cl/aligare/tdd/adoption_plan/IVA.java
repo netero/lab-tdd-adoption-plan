fbc2c33c085253e05ae53378c1c16faf970050f4
Rafael Morales <rmorales@aligare.cl>
2020-01-15 11:09:27 -0300

    primera prueba resuleta

---
+++ src/main/java/cl/aligare/tdd/adoption_plan/IVA.java
@@ -0,0 +1,5 @@
+package cl.aligare.tdd.adoption_plan;
+
+public class IVA {
+
+}
---
+++ src/test/java/cl/aligare/tdd/adoption_plan/IVATest.java
@@ -0,0 +1,10 @@
+package cl.aligare.tdd.adoption_plan;
+
+import org.junit.Test;
+
+public class IVATest {
+	@Test
+	public void classGeneration() {
+		IVA iva= new IVA();
+	}
+}
